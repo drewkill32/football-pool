7d8d7b56b6aa0e0b288b586578101ea5f04c1c96
Drew Killion <adk@killion.me>
2021-02-21 16:49:19 -0500

    modifying home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,21 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import React from 'react';
+import Picks from '../../components/Picks';
 import WeekSlider from '../../components/WeekSlider';
+import WeekContextProvider from '../../context/WeekContext.Provider';
+import { createStyles, Grid, makeStyles, Theme } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: 'flex',
       flexDirection: 'column',
-      justifyContent: 'center',
+      justifyContent: 'space-around',
       alignItems: 'center',
+      width: '100%',
+      height: '100%',
+    },
+    picks: {
+      flexGrow: 1,
       width: '100%',
     },
   })
@@ -17,10 +24,22 @@ const useStyles = makeStyles((theme: Theme) =>
 const Home = () => {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <WeekSlider />
-      <p>Hello</p>
-    </div>
+    <WeekContextProvider>
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        alignContent="center"
+        spacing={2}
+      >
+        <Grid item xs={12}>
+          <WeekSlider />
+        </Grid>
+        <Grid item xs={12}>
+          <Picks className={classes.picks} />
+        </Grid>
+      </Grid>
+    </WeekContextProvider>
   );
 };
 
