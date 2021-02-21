b2c5a5232409e6e2216922beefbb605ae83a12a3
Drew Killion <adk@killion.me>
2021-02-21 16:51:03 -0500

    removing warnings

--- src/components/Picks/index.tsx
+++ src/components/Picks/index.tsx
@@ -1,34 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import { useAuth } from "../../auth";
-import { useSelectedWeek } from "../../context/WeekContext.Provider";
-import PicksSkeleton from "./PicksSkeleton";
-import { Pick, Team } from "../../models";
+import { useAuth } from '../../auth';
+import { useSelectedWeek } from '../../context/WeekContext.Provider';
+import PicksSkeleton from './PicksSkeleton';
+import { Pick, Team } from '../../models';
 import {
   Checkbox,
   createStyles,
   Grid,
   makeStyles,
   Paper,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import { green } from "@material-ui/core/colors";
-import { CheckBox } from "@material-ui/icons";
 const sleep = (ms: number) =>
   new Promise((response) => setTimeout(response, ms));
 
 const useStyles = makeStyles(() =>
   createStyles({
     img: {
-      width: "45px",
-      height: "45px",
+      width: '45px',
+      height: '45px',
     },
     team: {
       flexGrow: 1,
     },
     score: {
-      marginRight: "10px",
+      marginRight: '10px',
     },
   })
 );
@@ -63,7 +60,6 @@ const TeamLine: React.FC<{
 };
 
 const Picks: React.FC<{ className: string | undefined }> = ({ className }) => {
-  const classes = useStyles();
   const [selectedWeek] = useSelectedWeek();
   const [picks, setPicks] = useState<Pick[]>();
   const [loading, setLoading] = useState(false);
@@ -75,7 +71,7 @@ const Picks: React.FC<{ className: string | undefined }> = ({ className }) => {
       setLoading(true);
       const url = `sample/db/picks-userid-${user?.id}.json`;
       try {
-        // await sleep(100);
+        await sleep(700);
         const response = await fetch(url);
         const data = (await response.json()) as Pick[];
         const thisWeeksPicks = data.filter(
