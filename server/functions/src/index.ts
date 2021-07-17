f42978792bc12e968ad297cda50dfb042414190b
Drew Killion <adk@killion.me>
2021-07-17 12:15:37 -0400

    fixing backend college football data

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,46 +1,50 @@
 import * as functions from 'firebase-functions';
-
 import axios from 'axios';
 
-const getSearchParams = (tbd: any) => {
-  const isURLSearchParams = (tbd: any): tbd is URLSearchParams => {
-    return true;
-  };
-  if (isURLSearchParams(tbd)) {
-    return tbd;
-  }
-  return new URLSearchParams({
-    year: '2020',
-  });
-};
+export const teams = functions.https.onRequest(async (request, response) => {
+  const urlParams = toURLSearchParams(request.query);
+  const { data } = await getFootballData('teams', urlParams);
 
-export const schedule = functions.https.onRequest(async (request, response) => {
-  var urlParams = getSearchParams(request.query);
-  console.log({ query: request.query, urlParams, str: urlParams.toString() });
-  const obj = {
-    prop1: 'hi there',
-    prop2: '100%',
-  };
-  const params = new URLSearchParams(obj).toString();
-  console.log(params);
-  const res = await axios.get(
-    `https://api.collegefootballdata.com/games?${urlParams.toString}`,
+  response.send(data);
+});
+
+export const games = functions.https.onRequest(async (request, response) => {
+  const urlParams = toURLSearchParams(request.query);
+  const { data } = await getFootballData('games', urlParams);
+
+  response.send(data);
+});
+
+export const calendar = functions.https.onRequest(async (request, response) => {
+  const urlParams = toURLSearchParams(request.query);
+  const { data } = await getFootballData('calendar', urlParams);
+
+  response.send(data);
+});
+export const conferences = functions.https.onRequest(
+  async (request, response) => {
+    const urlParams = toURLSearchParams(request.query);
+    const { data } = await getFootballData('conferences', urlParams);
+
+    response.send(data);
+  }
+);
+
+function getFootballData(endPoint: string, params: URLSearchParams) {
+  return axios.get(
+    `https://api.collegefootballdata.com/${endPoint}?${params.toString()}`,
     {
       headers: {
-        Authorization:
-          'Bearer 0BxU+G23e88DJ9BtcaJDJ2cqpR3Bs26CUMcItUntilpFUvQj4Zjgm7TznKjEMXvd',
+        Authorization: `Bearer ${functions.config().cfbd.key}`,
       },
     }
   );
-  const data = await res.data;
-  response.send({ data });
-});
+}
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-
-  response.send('Hello from Firebase2!');
-});
+function toURLSearchParams(params: any) {
+  const urlParams = new URLSearchParams();
+  for (const item in params) {
+    urlParams.append(item, `${params[item]}`);
+  }
+  return urlParams;
+}
